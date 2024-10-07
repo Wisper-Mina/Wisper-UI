200e0d56d6d20e527bfe3820e9da013be5a94603
Berkay Gündüz <gunduzberkay1532@gmail.com>
2024-10-07 18:26:31 +0300

    proof recursion and ecdh-pallas completed

--- mina/src/interfaces/MessagePackage.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Proof } from "o1js";
-import { EncryptedData } from "./message.interface";
-import { MessageProof } from "../structs/MessageProof";
-
-export interface MessagePackage {
-  encryptedMessage: EncryptedData;
-  proof: Proof< MessageProof, void>;
-}
--- mina/src/interfaces/message.interface.ts
+++
@@ -1,6 +0,0 @@
-// Interface for the encrypted data
-export interface EncryptedData {
-  iv: string;
-  encryptedData: string;
-  authTag: string;
-}
