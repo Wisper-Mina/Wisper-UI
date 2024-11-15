4017973f4891e44bff8f564591c258df0bf21a18
KkutaySarii <h.kutay.1@gmail.com>
2024-11-15 16:15:45 +0300

    config gile change ,

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,4 +1,7 @@
-import path from "path";
+import path from "node:path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -8,12 +11,8 @@ const nextConfig = {
     if (!isServer) {
       config.resolve.alias = {
         ...config.resolve.alias,
-        o1js: path.resolve(
-          import.meta.dirname,
-          "../../node_modules/o1js/dist/web/index.js"
-        ),
+        o1js: path.resolve(__dirname, "node_modules/o1js/dist/web/index.js"),
       };
-
       config.optimization.minimize = false;
     } else {
       config.externals.push("o1js"); // https://nextjs.org/docs/app/api-reference/next-config-js/serverExternalPackages
